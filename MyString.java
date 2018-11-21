914f64dce0699f90e1cfb40bfd2cf1a20c2040a8
asultan00 <asultan00@stuy.edu>
2018-11-21 12:01:02 -0500

    added and tested constructor, length, charAt, subSequence

--- MyString.java
+++ MyString.java
@@ -1,11 +1,26 @@
-public class MyString implements CharSequence,Comparable<CharSequence>{
+public class MyString implements CharSequence {
+     //need to add ,Comparable<CharSequence> to class declaration
+     public static void main(String[] args) {
+          MyString test = new MyString("fortnite");
+          System.out.println(test);
+          System.out.println(test.length());
+          System.out.println(test.charAt(2));
+          System.out.println(test.subSequence(0, 3));
+     }
      private char[] data;
      public MyString (CharSequence s) {
           data = new char[s.length()];
+          for (int i = 0; i < s.length(); i++) {
+               data[i] = s.charAt(i);
+          }
      }
 
      public String toString() {
-
+          String result = "";
+          for (int i = 0; i < data.length; i++) {
+               result = result + data[i];
+          }
+          return result;
      }
      //charAt() - takes index, prints character at that index
      public char charAt(int idx) {
@@ -17,5 +32,15 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
      //length() - returns length of CharSequence
      public int length() {
           return data.length;
+     }
+     public CharSequence subSequence(int start, int end) {
+          String result = "";
+          if (start < 0 || end >= data.length || start > end) {
+               throw new IndexOutOfBoundsException();
+          }
+          for (int i = start; i <= end; i++) {
+               result = result + data[i];
+          }
+          return result;
      }
 }
