726c14863e0b730c8d60236eed586a625748a318
asultan00 <asultan00@stuy.edu>
2018-11-26 07:09:57 -0500

    edited myString

--- MyString.java
+++ MyString.java
@@ -1,11 +1,12 @@
-public class MyString implements CharSequence {
-     //need to add ,Comparable<CharSequence> to class declaration
+public class MyString implements CharSequence,Comparable<CharSequence> {
      public static void main(String[] args) {
           MyString test = new MyString("fortnite");
           System.out.println(test);
-          System.out.println(test.length());
-          System.out.println(test.charAt(2));
-          System.out.println(test.subSequence(0, 3));
+          System.out.println(test.length()); //8
+          System.out.println(test.charAt(2)); //r
+          System.out.println(test.subSequence(0, 3)); //fort
+          System.out.println(test.compareTo("epic")); //4
+          System.out.println(test.compareTo("doublefortnite")); //-6
      }
 
      private char[] data;
@@ -56,15 +57,12 @@ public class MyString implements CharSequence {
      returns a negative integer, zero, or a positive integer as this object is less than, equal to,
      or greater than the specified object respectively */
 
-     /*
      public int compareTo(CharSequence o) {
           int diff = 0;
-          if (o.equals(null)) {
+          if (o == null) {
                throw new NullPointerException();
           }
-          if (o.getClass().equals)
+          diff = this.length() - o.length();
           return diff;
      }
-
-     */
 }
